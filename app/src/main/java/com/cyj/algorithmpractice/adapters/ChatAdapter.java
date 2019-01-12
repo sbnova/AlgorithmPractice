2b1d9244b04b2fe59adf7d99dbd542e7d7e3c81b
test <test1>
2019-01-12 14:11:34 +0900

    채팅 어댑터 데이터 출력마무리

--- app/src/main/java/com/cyj/algorithmpractice/adapters/ChatAdapter.java
+++ app/src/main/java/com/cyj/algorithmpractice/adapters/ChatAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.cyj.algorithmpractice.R;
 import com.cyj.algorithmpractice.datas.Chat;
@@ -30,6 +32,28 @@ public class ChatAdapter extends ArrayAdapter<Chat> {
 
         if(row == null){
             row = inf.inflate(R.layout.chat_list_item, null);
+        }
+
+        LinearLayout computerMsgLayout = row.findViewById(R.id.computerMsgLayout);
+        TextView computerMsgTxt = row.findViewById(R.id.computerMsgTxt);
+        LinearLayout userMsgLayout = row.findViewById(R.id.userMsgLayout);
+        TextView userMsgTxt = row.findViewById(R.id.userMsgTxt);
+
+        Chat data = mList.get(position);
+
+//        data가 가진 sender를 봤을때 computer / user 두가지 경우가 존재.
+        if(data.getSender().equals("user")){
+//            사용자가 입력한 메세지
+            computerMsgLayout.setVisibility(View.GONE);
+            userMsgLayout.setVisibility(View.VISIBLE);
+
+            userMsgTxt.setText(data.getMessage());
+        }else{
+//            컴퓨터가 입력한 메세지
+            userMsgLayout.setVisibility(View.GONE);
+            computerMsgLayout.setVisibility(View.VISIBLE);
+
+            computerMsgTxt.setText(data.getMessage());
         }
 
         return row;
