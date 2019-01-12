82d118a1840c388873c1a8dd522e6452dcafcf22
test <test1>
2019-01-12 10:20:55 +0900

    2단 버튼 이벤트 작성 완료

--- app/src/main/java/com/cyj/algorithmpractice/Question03Activity.java
+++ app/src/main/java/com/cyj/algorithmpractice/Question03Activity.java
@@ -1,9 +1,22 @@
 package com.cyj.algorithmpractice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Question03Activity extends BaseActivity {
+
+    private android.widget.Button twoBtn;
+    private android.widget.Button threeBtn;
+    private android.widget.Button fourBtn;
+    private android.widget.Button fiveBtn;
+    private android.widget.Button sixBtn;
+    private android.widget.Button sevenBtn;
+    private android.widget.Button eightBtn;
+    private android.widget.Button nineBtn;
+    private android.widget.Button totalBtn;
+    private android.widget.TextView resultTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,21 @@ public class Question03Activity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        twoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                2 * 1 = 2 ... 2 * 9 = 18 까지를 resultTxt에 출력
+
+//                이 String 변수를 가공해서 최종 결과를 resultTxt에 text로 지정
+                String  gugudanStr = "";
+
+                for(int i = 1; i <= 9 ; i++){
+                    gugudanStr += String.format("%d * %d = %d\n", 2, i, 2 * i);
+                }
+
+                resultTxt.setText(gugudanStr);
+            }
+        });
 
     }
 
@@ -27,6 +55,16 @@ public class Question03Activity extends BaseActivity {
 
     @Override
     public void bindViews() {
+        this.resultTxt = (TextView) findViewById(R.id.resultTxt);
+        this.totalBtn = (Button) findViewById(R.id.totalBtn);
+        this.nineBtn = (Button) findViewById(R.id.nineBtn);
+        this.eightBtn = (Button) findViewById(R.id.eightBtn);
+        this.sevenBtn = (Button) findViewById(R.id.sevenBtn);
+        this.sixBtn = (Button) findViewById(R.id.sixBtn);
+        this.fiveBtn = (Button) findViewById(R.id.fiveBtn);
+        this.fourBtn = (Button) findViewById(R.id.fourBtn);
+        this.threeBtn = (Button) findViewById(R.id.threeBtn);
+        this.twoBtn = (Button) findViewById(R.id.twoBtn);
 
     }
 }
